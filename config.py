60de95a51ce17a14681de1c38ee212ec13eb89b9
anticlimatic-xd <andremiguelmacute007@gmail.com>
2025-12-19 22:11:37 -0500

    Place the initial files

---
+++ config.py
@@ -0,0 +1,52 @@
+
+keyboard_sounds ={
+    "Typing": {
+        "Keyboard": (r"key_sounds\Default_press.mp3", r"key_sounds\Default_release.mp3"),
+        "Typewriter": (r"key_sounds\Typewriter_press.mp3", r"key_sounds\Typewriter_release.mp3")
+        },
+    
+    "Switches":{
+        "Cherry MX Blue": ()
+    },
+
+    "Abuse": {
+    "Torture": (r"key_sounds\Bonk_press.mp3", r"key_sounds\Ow_release.mp3"),
+    "Crack": (r"key_sounds\Crack_press.mp3", r"key_sounds\Crack_release.mp3")
+        },
+
+    "Fart": {
+    "Fart": (r"key_sounds\Fart_press.mp3", r"key_sounds\Fart_release.mp3"),
+    "Brainfart": (r"key_sounds\Brainfart_press.mp3", r"key_sounds\Brainfart_release.mp3")
+        },
+
+    "Speaking": {
+        "Fahh": (r"key_sounds\Fahh_press.mp3", r"key_sounds\Fahh_release.mp3"),
+        "Pluh": (r"key_sounds\Pluh_press.mp3", r"key_sounds\Pluh_release.mp3"),
+        "67": (r"key_sounds\67_press.mp3", r"key_sounds\67_release.mp3")
+    },
+
+    "Computer Sounds": {
+    "Windows Error": (r"key_sounds\Windowserror_press.mp3", r"key_sounds\Windowserror_release.mp3"),
+    "Discord": (r"key_sounds\Discord_press.mp3", r"key_sounds\Discord_release.mp3"),
+    },
+
+    "Unclassed": {
+    
+    "Money": (r"key_sounds\Money_press.mp3", r"key_sounds\Money_release.mp3")
+        }
+    }
+
+
+window_flags = {
+    "Volume": False,
+    "Custom": False,
+    "Soundpack": False,
+
+}
+is_running = False
+
+volume = 20
+
+SPADX = 2
+BPADX = 30
+PADY = 10
